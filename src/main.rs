2b0e99f34d916ec65eb8dc52689745b53ec1d870
dmackdev <dmackdev@gmail.com>
2022-08-21 12:03:00 +0100

    initialising market with cards from deck

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ use std::iter;
 fn main() {
     App::new()
         .init_resource::<Deck>()
-        .add_startup_system(debug_deck)
+        .init_resource::<Market>()
+        .add_startup_system(debug_market)
+        .add_startup_system(debug_deck.after(debug_market))
         .run();
 }
 
 fn debug_deck(deck: Res<Deck>) {
+    println!("Deck:");
     for card in deck.cards.iter() {
+        println!("{:?}", card);
+    }
+}
+
+fn debug_market(market: Res<Market>) {
+    println!("Market:");
+    for card in market.cards.iter() {
         println!("{:?}", card);
     }
 }
@@ -86,5 +96,38 @@ impl FromWorld for Deck {
         cards.shuffle(&mut rng);
 
         Self { cards }
+    }
+}
+
+struct Market {
+    cards: Vec<Card>,
+}
+
+impl FromWorld for Market {
+    fn from_world(world: &mut World) -> Self {
+        // take 3 camel tokens from deck, and 2 random cards to fill the market
+        let mut market_cards = vec![];
+
+        let mut deck = world.get_resource_mut::<Deck>().unwrap();
+
+        for _ in 0..3 {
+            let camel_card_idx = deck
+                .cards
+                .iter()
+                .position(|c| matches!(c.0, CardType::Camel))
+                .unwrap();
+
+            let camel_card = deck.cards.remove(camel_card_idx);
+            market_cards.push(camel_card);
+        }
+
+        for _ in 0..2 {
+            let random_card = deck.cards.remove(0);
+            market_cards.push(random_card);
+        }
+
+        Self {
+            cards: market_cards,
+        }
     }
 }
